3a02d141b437b2a2917ada3daf8ae7997fbebda6
joshxyzhimself <60829711+joshxyzhimself@users.noreply.github.com>
2022-04-01 08:35:41 +0800

    save

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// @ts-check
+
+import data from './data.json';
+
+console.log({ data });
+
+const content = document.getElementById('content');
+const section = document.createElement('div')
+section.classList.add('section');
